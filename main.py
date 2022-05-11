9b8e06f5a95f616a0ce9d7ef444940e91ccdb4bd
Error <88781785+TheSadError@users.noreply.github.com>
2022-05-11 17:40:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import time
+import time 
 from colorama import Fore,Back,Style
 
 def menu():
@@ -9,20 +9,143 @@ def menu():
 
         [M] Main
         [1] Web Hacking & Scanning Tools
+        [2] Brute Forcer Tools
 
     """)
 
 
-def DarkLight():
+def void():
     os.system("clear")
 
     menu()
     n = input(Fore.RED+"[+] Please Select : ")
     if n=="1":
         os.system("sudo python3 ./core/web/main.py")
+    if n=="2":
+        os.system("sudo python3 ./core/brute/main.py")
     if n=="M" or n == "m":
         os.system("sudo python3 main.py")
 
 
 if __name__ == "__main__":
-    DarkLight()
+    void()import os
+import time 
+from colorama import Fore,Back,Style
+
+def menu():
+    print(Fore.BLUE+"""
+        
+        [+] VOID [+]
+
+        [M] Main
+        [1] Web Hacking & Scanning Tools
+        [2] Brute Forcer Tools
+
+    """)
+
+
+def void():
+    os.system("clear")
+
+    menu()
+    n = input(Fore.RED+"[+] Please Select : ")
+    if n=="1":
+        os.system("sudo python3 ./core/web/main.py")
+    if n=="2":
+        os.system("sudo python3 ./core/brute/main.py")
+    if n=="M" or n == "m":
+        os.system("sudo python3 main.py")
+
+
+if __name__ == "__main__":
+    void()import os
+import time 
+from colorama import Fore,Back,Style
+
+def menu():
+    print(Fore.BLUE+"""
+        
+        [+] VOID [+]
+
+        [M] Main
+        [1] Web Hacking & Scanning Tools
+        [2] Brute Forcer Tools
+
+    """)
+
+
+def void():
+    os.system("clear")
+
+    menu()
+    n = input(Fore.RED+"[+] Please Select : ")
+    if n=="1":
+        os.system("sudo python3 ./core/web/main.py")
+    if n=="2":
+        os.system("sudo python3 ./core/brute/main.py")
+    if n=="M" or n == "m":
+        os.system("sudo python3 main.py")
+
+
+if __name__ == "__main__":
+    void()import os
+import time 
+from colorama import Fore,Back,Style
+
+def menu():
+    print(Fore.BLUE+"""
+        
+        [+] VOID [+]
+
+        [M] Main
+        [1] Web Hacking & Scanning Tools
+        [2] Brute Forcer Tools
+
+    """)
+
+
+def void():
+    os.system("clear")
+
+    menu()
+    n = input(Fore.RED+"[+] Please Select : ")
+    if n=="1":
+        os.system("sudo python3 ./core/web/main.py")
+    if n=="2":
+        os.system("sudo python3 ./core/brute/main.py")
+    if n=="M" or n == "m":
+        os.system("sudo python3 main.py")
+
+
+if __name__ == "__main__":
+    void()import os
+import time 
+from colorama import Fore,Back,Style
+
+def menu():
+    print(Fore.BLUE+"""
+        
+        [+] VOID [+]
+
+        [M] Main
+        [1] Web Hacking & Scanning Tools
+        [2] Brute Forcer Tools
+
+    """)
+
+
+def void():
+    os.system("clear")
+
+    menu()
+    n = input(Fore.RED+"[+] Please Select : ")
+    if n=="1":
+        os.system("sudo python3 ./core/web/main.py")
+    if n=="2":
+        os.system("sudo python3 ./core/brute/main.py")
+    if n=="M" or n == "m":
+        os.system("sudo python3 main.py")
+
+
+if __name__ == "__main__":
+    void()
